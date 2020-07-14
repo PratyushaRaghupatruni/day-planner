b656b47bbfcfdfbd7964a9371b379511e000ab1e
pratyu <pratyusha.raghupatruni@gmail.com>
2020-07-13 21:27:43 -0700

    localstorage is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 
-var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-
 
 //to fetch the date month and year 
-var date = moment().format("dddd MMMM Mo YYYY");
-console.log(moment().format("dddd MMMM Mo YYYY"));
-$("#currentDay").append(date);
+function displayDate() {
+    var date = moment().format("dddd MMMM Mo YYYY");
+    console.log(moment().format("dddd MMMM Mo YYYY"));
+    $("#currentDay").append(date);
+    $("#currentDay").addClass("font-weight-bold text-info");
+}
 
 var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-updateTime();
 function updateTime() {
     var currentHour = moment().format("H");
     console.log(currentHour);
@@ -17,21 +17,40 @@ function updateTime() {
         console.log(timeOfday[i]);
         if (timeOfday[i] > currentHour) {
             $("#" + timeOfday[i]).attr("style", " background-color: #77dd77");
-            console.log("coming hour");
         }
-         if (timeOfday[i] < currentHour) {
-            $("#" + timeOfday[i]).attr("style", "background-color: #d3d3d3");
-            console.log("previous hour");
+        if (timeOfday[i] < currentHour) {
+            $("#" + timeOfday[i]).attr("style", "background-color: #d3d3d3")
         }
         if (timeOfday[i] === currentHour) {
             $("#" + timeOfday[i]).attr("style", "background-color: #ff696");
-            console.log("current hour");
+        }
+    }
+}
+var listOfevents = [];
+$(".saveBtn").on("click", function () {
+    var userText = $(this).parent().children("textarea").val();
+    console.log(userText);
+    var userTime = $(this).parent().attr("id");
+    console.log(userTime);
 
-        }//end of if loop
-    }//end of for loop
-}//end of function
-$(".savebtn").on("click", function () {
-    var userText = $(this).text();
-    console.log($(this).text());
-    // var userTime = $(this)
+    var dayOfevents = {
+        eventText: userText,
+        eventTime: userTime,
+    }
+
+    if (userText==="") {
+        alert("enter the list of events");
+    }
+    else if(userText!==""){
+     localStorage.setItem("eventDetails",JSON.stringify(dayOfevents));  
+     var details = JSON.parse(localStorage.getItem("eventDetails"));   
+    }
+
+    
+
+    
+
+
 });
+displayDate();
+updateTime();
