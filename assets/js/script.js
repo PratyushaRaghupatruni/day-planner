67c377382ac2b573e49ea2d52a7404102d78a023
pratyu <pratyusha.raghupatruni@gmail.com>
2020-07-13 18:14:56 -0700

    changed ifelse

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,24 +9,23 @@ $("#currentDay").append(date);
 
 var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 updateTime();
-
 function updateTime() {
-    var currentHour = moment().format("HA");
+    var currentHour = moment().format("H");
     console.log(currentHour);
     for (i = 0; i < timeOfday.length; i++) {
-        timeOfday[i]=parseInt(timeOfday[i]);
+        timeOfday[i] = parseInt(timeOfday[i]);
         console.log(timeOfday[i]);
-       if (timeOfday[i] > currentHour) {
-           $("#" + timeOfday[i]).attr("style", " background-color: #77dd77");
-           console.log(timeOfday[i]);
-       }
-        else if (timeOfday[i] < currentHour) {
-            $("#" + timeOfday[i]).attr("style", "background-color: #d3d3d3");
-            console.log($timeOfday[i]);
+        if (timeOfday[i] > currentHour) {
+            $("#" + timeOfday[i]).attr("style", " background-color: #77dd77");
+            console.log("coming hour");
         }
-        else if (timeOfday[i] === currentHour) {
+         if (timeOfday[i] < currentHour) {
+            $("#" + timeOfday[i]).attr("style", "background-color: #d3d3d3");
+            console.log("previous hour");
+        }
+        if (timeOfday[i] === currentHour) {
             $("#" + timeOfday[i]).attr("style", "background-color: #ff696");
-            console.log(timeOfday[i]);
+            console.log("current hour");
 
         }//end of if loop
     }//end of for loop
@@ -34,5 +33,5 @@ function updateTime() {
 $(".savebtn").on("click", function () {
     var userText = $(this).text();
     console.log($(this).text());
-   // var userTime = $(this)
+    // var userTime = $(this)
 });
